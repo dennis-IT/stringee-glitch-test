025064073016f03c40f7dca7fcd37021291759bc
Glitch (hello-webpage) <none>
2020-10-30 02:42:18 +0000

    🐝⛪️ Checkpoint ./script.js:24457169/3950

--- script.js
+++ script.js
@@ -1,54 +1,69 @@
+const videoContainer = document.querySelector('#videos');
+
 const vm = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    userToken: '',
-    roomToken: '',
-    roomId: '',
+    userToken: "",
+    roomToken: "",
+    roomId: "",
     room: undefined,
     client: undefined
   },
   mounted() {
-    api.setRestToken()
+    api.setRestToken();
   },
   methods: {
     login: function() {
       //Create a promise
-      return new Promise(async resolve );
-      
-      //Create a random userId
-      const userId = (Math.random() * 10000).toFixed(0);
-      
-      //Get user Token from userId
-      const userToken = await api.getUserToken(userId);
-      this.userToken = userToken;
-      
-      //Connect to stringee
-      const client = new StringeeClient();
-      client.on('authen', (result) => {
-        console.log('on authen', result);
+      return new Promise(async resolve => {
+        //Create a random userId
+        const userId = (Math.random() * 10000).toFixed(0);
+
+        //Get user Token from userId
+        const userToken = await api.getUserToken(userId);
+        this.userToken = userToken;
+
+        //Connect to stringee
+        const client = new StringeeClient();
+        client.on("authen", result => {
+          console.log("on authen", result);
+          resolve(result);
+        });
+
+        client.connect(userToken);
+
+        this.client = client;
       });
-      
-      client.connect(userToken);
+    },
+    publishVideo: async function() {
+      const localTrack = await StringeeVideo.createLocalVideoTrack(this.client, {
+        audio: true,
+        video: true,
+        videoDimensions: {width: 640, height: 360}
+      });
     },
     createRoom: async function() {
-      console.log('create room');
+      console.log("create room");
       const room = await api.createRoom();
       const roomToken = await api.getRoomToken(room.roomId);
-      
+
       //Assign roomToken to data
       this.roomId = room.roomId;
       this.roomToken = roomToken;
+      
+      await login();
+      
     },
     joinRoom: async function() {
-      const roomId = prompt('Paste room Id here');
-      
-      if(!roomId) {
+      const roomId = prompt("Paste room Id here");
+
+      if (!roomId) {
         return;
       }
-      
-      const roomToken = await api.getRoomToken(roomId);      
+
+      const roomToken = await api.getRoomToken(roomId);
       this.roomId = roomId;
       this.roomToken = roomToken;
     }
   }
-})
+});
