0ce9b5a2f696050ffecf466642f81ce5b02da3ba
Glitch (hello-webpage) <none>
2020-10-30 03:40:26 +0000

    🍩🏃 Checkpoint ./script.js:24457169/6470

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const videoContainer = document.querySelector('#videos');
+const videoContainer = document.querySelector("#videos");
 
 const vm = new Vue({
   el: "#app",
@@ -36,51 +36,52 @@ const vm = new Vue({
       });
     },
     publishVideo: async function() {
-      const localTrack = await StringeeVideo.createLocalVideoTrack(this.client, {
-        audio: true,
-        video: true,
-        videoDimensions: {width: 640, height: 360}
-      });
-      
+      const localTrack = await StringeeVideo.createLocalVideoTrack(
+        this.client,
+        {
+          audio: true,
+          video: true,
+          videoDimensions: { width: 640, height: 360 }
+        }
+      );
+
       const videoElement = localTrack.attach();
       videoContainer.appendChild(videoElement);
-      
+
       //Start to join a room
-      const roomData = await StringeeVideo.joinRoom(this.client, this.roomToken);
-      
+      const roomData = await StringeeVideo.joinRoom(
+        this.client,
+        this.roomToken
+      );
+
       const room = roomData.room;
-      console.log({roomData, room});
+      console.log({ roomData, room });
       this.room = room;
-      
+
       room.clearAllOnMethos();
-      
+
       room.on("addtrack", async event => {
         const trackInfo = event.info.track;
-        
-        if(trackInfo.serverId === localTrack.serverId) {
+
+        if (trackInfo.serverId === localTrack.serverId) {
           return;
         }
         
-        const track = await room.subscribe(trackInfo.serverId);
-        
-        track.on("ready", () => {
-          const element = track.attach();
-          videoContainer.appendChild(element);
-        });
-        
+        this.subscribeTrack(trackInfo);
+
         room.on("removetrack", event => {
-          if(!event.track) {
+          if (!event.track) {
             return;
           }
-          
+
           const elements = event.track.detach();
-          
+          elements.forEach(element => element.remove());
         });
         
+        //Display all trackinfo in a room
+        roomData.listTracksInfo.forEach(trackInfo => this.subscribeTrack(trackInfo));
       });
-      
-      
-      
+
       room.publish(localTrack);
     },
     createRoom: async function() {
@@ -91,10 +92,9 @@ const vm = new Vue({
       //Assign roomToken to data
       this.roomId = room.roomId;
       this.roomToken = roomToken;
-      
+
       await this.login();
       await this.publishVideo();
-      
     },
     joinRoom: async function() {
       const roomId = prompt("Paste room Id here");
@@ -106,9 +106,17 @@ const vm = new Vue({
       const roomToken = await api.getRoomToken(roomId);
       this.roomId = roomId;
       this.roomToken = roomToken;
-      
+
       await this.login();
       await this.publishVideo();
     }
+  },
+  subscribeTrack: async function(trackInfo) {
+    const track = await room.subscribe(trackInfo.serverId);
+
+    track.on("ready", () => {
+      const element = track.attach();
+      videoContainer.appendChild(element);
+    });
   }
 });
