e60da4299809b785975937a24fbf5eb3b6212f8c
Glitch (hello-webpage) <none>
2020-10-30 02:19:57 +0000

    👆✈️ Checkpoint ./index.html:24457169/248 ./script.js:24457169/799

--- script.js
+++ script.js
@@ -9,5 +9,27 @@ const vm = new Vue({
   },
   mounted() {
     api.setRestToken()
+  },
+  methods: {
+    createRoom: async function() {
+      console.log('create room');
+      const room = await api.createRoom();
+      const roomToken = await api.getRoomToken(room.roomId);
+      
+      //Assign roomToken to data
+      this.roomId = room.roomId;
+      this.roomToken = roomToken;
+    },
+    joinRoom: async function() {
+      const roomId = prompt('Paste room Id here');
+      
+      if(!roomId) {
+        return;
+      }
+      
+      const roomToken = await api.getRoomToken(roomId);      
+      this.roomId = roomId;
+      this.roomToken = roomToken;
+    }
   }
 })
